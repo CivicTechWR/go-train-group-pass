d924d93338c249c0f5e9eb0f3d67c59f44613dbb
jliu1016 <jess@Mac.localdomain>
2025-11-13 01:05:30 -0500

    use repository

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
 import { User } from '../entities/user.entity';
 
 @Module({
   imports: [MikroOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
