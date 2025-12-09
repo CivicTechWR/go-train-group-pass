6fa1bbd2d769a2962f04c9ec8ab519af3697bf88
jliu1016 <jess@Mac.localdomain>
2025-12-08 23:57:42 -0500

    add get existing itineraries

--- backend/src/itineraries/itineraries.controller.ts
+++ backend/src/itineraries/itineraries.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   Request,
   UnauthorizedException,
@@ -13,6 +14,9 @@ import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Serialize } from '../common/decorators/serialize.decorator';
 import {
   CreateItineraryDto,
+  ExistingItinerariesDto,
+  ExistingItinerariesSchema,
+  ItineraryCreationResponseDto,
   ItineraryCreationResponseSchema,
 } from '@go-train-group-pass/shared';
 
@@ -31,10 +35,18 @@ export class ItinerariesController {
   async create(
     @Request() req: RequestWithUser,
     @Body() createItineraryDto: CreateItineraryDto,
-  ) {
+  ): Promise<ItineraryCreationResponseDto> {
     if (!req.user) {
       throw new UnauthorizedException('User not found');
     }
     return this.itinerariesService.create(req.user.id, createItineraryDto);
+  }
+
+  @Get('existing')
+  @ApiOperation({ summary: 'Summary of all itineraries with same trips' })
+  @ApiOkResponse({ description: 'Existing itineraries' })
+  @Serialize(ExistingItinerariesSchema)
+  async getExistingItineraries(): Promise<ExistingItinerariesDto> {
+    return this.itinerariesService.getExistingItineraries();
   }
 }
--- packages/shared/src/itinerary/itinerary.dto.ts
+++ packages/shared/src/itinerary/itinerary.dto.ts
@@ -1,5 +1,7 @@
-import { ItineraryCreationResponseSchema, CreateItinerarySchema } from "./itinerary.schemas";
+import { ItineraryCreationResponseSchema, CreateItinerarySchema, ExistingItinerariesSchema, ExistingItinerarySchema } from "./itinerary.schemas";
 import { createZodDto } from "nestjs-zod";
 
 export class ItineraryCreationResponseDto extends createZodDto(ItineraryCreationResponseSchema) {}
 export class CreateItineraryDto extends createZodDto(CreateItinerarySchema) {}
+export class ExistingItinerariesDto extends createZodDto(ExistingItinerariesSchema) {}
+export class ExistingItineraryDto extends createZodDto(ExistingItinerarySchema) {}
